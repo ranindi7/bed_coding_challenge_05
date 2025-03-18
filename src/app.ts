0a66ed7189400250439c278496cd55f1cfecbda2
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-03-18 14:31:40 -0500

    Completed ticket 2 manging environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import moderationRoutes from "./api/v1/routes/moderationRoutes";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
 const app: Express = express();
 app.use(express.json());
+dotenv.config();
+
+const LOG_LEVEL = process.env.LOG_LEVEL || "dev";
+app.use(morgan(LOG_LEVEL));
 
 /**
  * Mount moderation routes on /api/v1/moderation
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import app from "./app";
 import { Server } from "http";
 
 const PORT: string | number = process.env.PORT || 3000;
+const NODE_ENV: string = process.env.NODE_ENV || "development";
 
 const server: Server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `Server running in development mode on port ${PORT} in ${NODE_ENV} environment`
+  );
 });
 
 export default server;
